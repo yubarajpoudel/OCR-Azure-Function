f793b1a149dc7f6f48f3b4198996209967f67009
Yubaraj Poudel <yubaraj.p@xagroup.com>
2022-04-12 01:52:50 +0400

    updated

--- OpenCVHttpTrigger/imageread.py
+++ OpenCVHttpTrigger/imageread.py
@@ -59,7 +59,7 @@ class VinOcr:
             cropped = im2[y:y + h, x:x + w]
 
             # Apply OCR on the cropped image
-            text = pytesseract.image_to_string(cropped, config="--psm 7")
+            text = pytesseract.image_to_string(cropped, lang='eng', config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
             recognized_texts.append(text)
         print(f"recognized_text = {len(recognized_texts)}")
         #return json.dumps(recognized_texts)
