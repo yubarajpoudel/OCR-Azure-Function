9ae3ef2cef54abcb1feab47580deebec5c5542a5
Yubaraj Poudel <yubaraj.p@xagroup.com>
2022-04-12 00:05:56 +0400

    pytesseract added

--- OpenCVHttpTrigger/__init__.py
+++ OpenCVHttpTrigger/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import azure.functions as func
+from OpenCVHttpTrigger.imageread import VinOcr
 
 import cv2
 import numpy as np
@@ -15,14 +16,14 @@ def loadImageFromRequestBody (
 # https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga04723e007ed888ddf11d9ba04e2232de
 # cv.Canny(image, threshold1, threshold2[, edges[, apertureSize[, L2gradient]]]) -> edges
 # cv.Canny(dx, dy, threshold1, threshold2[, edges[, L2gradient]]) -> edges
-def extractEdges (
-        buf: [np.uint8],
-        threshold1: int,
-        threshold2: int) -> [np.uint8]:
-    """Tranform the input image to show its edges using the Canny algorithm."""
-    img = cv2.imdecode(buf, cv2.IMREAD_GRAYSCALE)
-    img_edges = cv2.Canny(img, threshold1, threshold2)
-    return img_edges
+# def extractEdges (
+#         buf: [np.uint8],
+#         threshold1: int,
+#         threshold2: int) -> [np.uint8]:
+#     """Tranform the input image to show its edges using the Canny algorithm."""
+#     img = cv2.imdecode(buf, cv2.IMREAD_GRAYSCALE)
+#     img_edges = cv2.Canny(img, threshold1, threshold2)
+#     return img_edges
     
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -32,12 +33,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     THRESHOLD2 = 60
     
     img_buffer = loadImageFromRequestBody(req)
-    img_edges = extractEdges(
-        img_buffer, THRESHOLD1, THRESHOLD2)
+    # img_edges = extractEdges(
+    #     img_buffer, THRESHOLD1, THRESHOLD2)
 
-    img_encoded = cv2.imencode('.jpg', img_edges)
-    img_response = img_encoded[1].tobytes()
+    # img_encoded = cv2.imencode('.jpg', img_edges)
+    # img_response = img_encoded[1].tobytes()
+    vin_ocr = VinOcr()
 
+    vin_number = vin_ocr.get_vin_number(img_buffer)
     # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
     headers = {
         'Content-Type': 'image/jpeg',
@@ -46,6 +49,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     }
 
     return func.HttpResponse(
-        body=img_response,
+        body= vin_number,
         headers=headers,
         status_code=200)
---
+++ OpenCVHttpTrigger/imageread.py
@@ -0,0 +1,70 @@
+import cv2
+import pytesseract
+import re
+ 
+# Mention the installed location of Tesseract-OCR in your system
+#pytesseract.pytesseract.tesseract_cmd = 'System_path_to_tesseract.exe'
+ 
+# Read image from which text needs to be extracted
+class VinOcr:
+
+    def __init__(self) -> None:
+        pass
+
+    def get_vin_number(imageFile):
+        img = cv2.imread(imageFile)
+        
+        # Preprocessing the image starts
+        
+        # Convert the image to gray scale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        # Performing OTSU threshold
+        ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+        
+        # Specify structure shape and kernel size.
+        # Kernel size increases or decreases the area
+        # of the rectangle to be detected.
+        # A smaller value like (10, 10) will detect
+        # each word instead of a sentence.
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+        
+        # Applying dilation on the threshold image
+        dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+        
+        # Finding contours
+        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                                        cv2.CHAIN_APPROX_NONE)
+        
+        # Creating a copy of image
+        im2 = img.copy()
+
+        # Looping through the identified contours
+        # Then rectangular part is cropped and passed on
+        # to pytesseract for extracting text from it
+        # Extracted text is then written into the text file
+        recognized_texts = []
+        for cnt in contours:
+            x, y, w, h = cv2.boundingRect(cnt)
+            #print(f'{x,y,w,h}')
+            
+            # Drawing a rectangle on copied image
+            rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            
+            # Cropping the text block for giving input to OCR
+            cropped = im2[y:y + h, x:x + w]
+
+            # Apply OCR on the cropped image
+            text = pytesseract.image_to_string(cropped)
+            recognized_texts.append(text)
+
+        #Apply Regex to get the Vin Number
+        pattern =  r"[a-zA-Z0-9]{17}"
+
+        recognized_texts_in_string = ' '.join(recognized_texts)
+        #print(recognized_texts_in_string)
+        vin_number = re.search(pattern, recognized_texts_in_string).group()
+
+        print(f"Vin Number = {vin_number}")
+        return vin_number
+    
